8309f9e6bada6abe7178d48f772074a2c011391c
guillermoFragachan <guillefragachan@gmail.com>
2021-11-09 15:25:17 +0100

    CRUD

--- src/services/blogspots/index.js
+++ src/services/blogspots/index.js
@@ -6,12 +6,35 @@ import blogpostSchema from "./shema.js";
 const router = express.Router();
 
 
+router.post("/", async (req, res, next) => {
+    
+      const newBlog = new blogpostSchema(req.body) 
+      const { _id } = await newBlog.save() 
+      res.status(201).send({ _id })
+    
+  })
 
-
-router.get("/", (req, res) => {
-    res.send("Hello from blogspots");
+router.get("/", async(req, res) => {
+    const blogs = await blogpostSchema.find()
+    res.send(blogs)
 });
 
+
+router.get("/:id", async(req, res) => {
+    const blog = await blogpostSchema.findById(req.params.id)
+    res.send(blog)
+});
+
+
+router.put("/:id", async(req, res) => {
+    const blog = await blogpostSchema.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    res.send(blog)
+});
+
+router.delete("/:id", async(req, res) => {
+    const blog = await blogpostSchema.findByIdAndDelete(req.params.id)
+    res.send(blog)
+});
 
 
 
--- src/services/blogspots/shema.js
+++ src/services/blogspots/shema.js
@@ -8,9 +8,7 @@ const blogpostSchema = new Schema(
       category: { type: String, required: true },
       title: { type: String, required: true },
       cover: { type: String, required: true },
-        content: { type: String, required: true },
-      age: { type: Number, min: 18, max: 65, required: true },
-      professions: [String],
+        content: { type: String, required: true }
     },
     {
       timestamps: true,
